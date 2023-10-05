9d253b67af769fbdef13c5dce54b769e049b3a91
AndreyLytvynov <mrjastonehome@gmail.com>
2023-10-05 23:00:04 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://finance-control-mu.vercel.app'],
-    allowedHeaders: ['Accept', 'Content-Type'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-  });
   await app.listen(3002);
 }
 bootstrap();
