8b29e0406e310bdb30d46c2a6ee92e719e2e653b
AndreyLytvynov <mrjastonehome@gmail.com>
2023-10-06 01:42:33 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-  // app.useGlobalPipes(new ValidationPipe());
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   // app.enableCors({
   //   origin: ['http://localhost:3000', 'https://finance-control-mu.vercel.app'],
   //   allowedHeaders: ['Accept', 'Content-Type'],
@@ -35,27 +33,6 @@ async function bootstrap() {
   //   methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
   //   credentials: true,
   // });
-
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
-
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
-
-  app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
-  });
-
-  await app.listen(3000);
+  await app.listen(3002);
 }
-
 bootstrap();
