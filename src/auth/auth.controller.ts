2897da58386b7bf2c177f1dd9077ab49b1891497
AndreyLytvynov <mrjastonehome@gmail.com>
2023-10-05 18:33:18 +0300

    fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { SignUpDto } from './dto/signup.dto';
 import { LoginDto } from './dto/login.dto';
+import { SignUpDto } from './dto/signUp.dto';
 
 @Controller('auth')
 export class AuthController {
