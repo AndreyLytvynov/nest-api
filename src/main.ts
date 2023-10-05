1417287603e20e5d3d80d97b204c5cbe52d4fdf8
AndreyLytvynov <mrjastonehome@gmail.com>
2023-10-05 19:16:46 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as cors from 'cors'; // Импорт cors
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.use(cors());
+  app.use(
+    cors({
+      origin: '*',
+    }),
+  );
   await app.listen(3002);
 }
 bootstrap();
