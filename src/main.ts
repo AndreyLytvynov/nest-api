74e1f17a48e4b7cc9f961fe80ccbc4380254d474
AndreyLytvynov <mrjastonehome@gmail.com>
2023-10-06 01:40:49 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  // const app = await NestFactory.create(AppModule);
+  // app.useGlobalPipes(new ValidationPipe());
   // app.enableCors({
   //   origin: ['http://localhost:3000', 'https://finance-control-mu.vercel.app'],
   //   allowedHeaders: ['Accept', 'Content-Type'],
@@ -13,26 +15,47 @@ async function bootstrap() {
   //   optionsSuccessStatus: 204,
   //   credentials: true,
   // });
-  app.enableCors({
-    origin: function (origin, callback) {
-      if (
-        [
-          'https://finance-control-mu.vercel.app',
-          'http://localhost:3000',
-        ].indexOf(origin) !== -1
-      ) {
-        console.log('allowed cors for:', origin);
-        callback(null, true);
-      } else {
-        console.log('blocked cors for:', origin);
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
-    credentials: true,
+  // app.enableCors({
+  //   origin: function (origin, callback) {
+  //     if (
+  //       [
+  //         'https://finance-control-mu.vercel.app',
+  //         'http://localhost:3000',
+  //       ].indexOf(origin) !== -1
+  //     ) {
+  //       console.log('allowed cors for:', origin);
+  //       callback(null, true);
+  //     } else {
+  //       console.log('blocked cors for:', origin);
+  //       callback(new Error('Not allowed by CORS'));
+  //     }
+  //   },
+  //   allowedHeaders:
+  //     'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+  //   methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+  //   credentials: true,
+  // });
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
   });
-  await app.listen(3002);
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
+
+  await app.listen(3000);
 }
+
 bootstrap();
