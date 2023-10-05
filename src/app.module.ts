3457a7c613d2f10618ea47cc7d78fe34a9681853
AndreyLytvynov <mrjastonehome@gmail.com>
2023-10-05 19:03:51 +0300

    fix cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
